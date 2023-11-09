6aae15de2722b5a278434947760f1fa1d6daeb0c
Razmo <you@example.com>
2023-11-09 15:29:40 +0100

    ADDED: ex_08, reordert colors

---
+++ JS_Racing_JS_exercices/JS_Racing_JS_exercices/script/ex_08.js
@@ -0,0 +1,21 @@
+//document.querySelectorAll('canvas:nth-of-type(3n+2)').style.backgroundColour = "#000"; 
+//document.getElementsByTagName("footer")[1].getElementsByTagName("canvas")[1].style.backgroundColor = "#000"  
+/*let canvas = document.querySelectorAll('canvas:hover~canvas:nth-of-type(3n+3)');
+    canvas.forEach(function(canvas){
+        canvas.style.backgroundColour = '#000';
+    }) */
+    window.onload=()=>{
+        //document.getElementsByTagName("footer")[0].getElementsByTagName("canvas")[0]. 
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[0].style.backgroundColor = "orange";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[1].style.backgroundColor = "orange";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[2].style.backgroundColor = "orange";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[3].style.backgroundColor = "purple";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[4].style.backgroundColor = "purple";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[5].style.backgroundColor = "purple";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[6].style.backgroundColor = "black";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[7].style.backgroundColor = "black";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[8].style.backgroundColor = "black";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[9].style.backgroundColor = "olive";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[10].style.backgroundColor = "olive";
+        document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("canvas")[11].style.backgroundColor = "olive";
+        }
